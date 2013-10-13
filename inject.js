c9258b74c973f22ef4e21d343535943a6a01c639
indraneel <indraneelpurohit@gmail.com>
2013-10-13 04:18:54 -0400

    blurred viiiines

--- inject.js
+++ inject.js
@@ -1,63 +1,4 @@
 (function($) {
-    var getBgImage = function(sel) {
-        var bgImg = $(sel).css('background-image');
-        var m = bgImg.match(/url\((.*?)\)/);
-        if(m.length > 0) {
-            return m[1];
-        }
-        
-        return undefined;
-    };
-
-    var kickoffDownload = function(url) {
-        if(url != undefined) {
-            /*chrome.extension.sendRequest({
-                "action": "download",
-                "opts": {"url": url}
-            });*/
-            var html = '<img src="' + url + '" width="76" alt="" />';
-
-            var div = $('<div />').css( {
-                'position': 'fixed',
-                'top': '0px',
-                'right': '16px',
-                'width': '76px',
-                'height': '76px',
-                'overflow': 'hidden',
-                'z-index': '100000',
-                'border': '3px solid #ccc'   
-            });
-
-            $(div).attr('id', 'instagramarator');
-            $(div).html(html);
-            $('body').append(div);
-        }else {
-            console.error('Invalid undefined url passed to kickoffDownload');
-        }
-    };
-
-    var findImageAndDownload = function(sel) {
-        kickoffDownload(getBgImage(sel));
-    };
-
-    var pageProfile = function() {
-        setTimeout( function() {
-            //This is an image popup from the profile page
-            if($('#profile-media-modal').is(':visible')) {
-                console.log(getBgImage('#profile-media-modal .imgImg'));
-                findImageAndDownload('#profile-media-modal .imgImg');
-            }else {
-                console.log('remove?');
-                $('#instagramarator').remove();
-            }
-        }, 500);
-    };
-
-    var pageMedia = function() {
-        console.log(getBgImage('.media-photo .img'));
-        findImageAndDownload('.media-photo .img');
-    };
-
     $(document).ready( function() {
         /*chrome.extension.sendRequest({
           "greeting": "hello",
@@ -65,15 +6,30 @@
           "var2": true           //boolean
         });*/
         console.log('ready');
+	function blur(i) {
+	    console.log(i);
+	    var blurs = [5, 25, 50];
+	    console.log(blurs[i]);
+	    var style = document.createElement('style');
+	    style.rel = 'stylesheet';
+	    style.type = 'text/css';
+	    var css = "#post { -webkit-filter: blur("+blurs[i]+"px);}";
+	    if (style.styleSheet){
+		  style.styleSheet.cssText = css;
+	    } else {
+		  style.appendChild(document.createTextNode(css));
+	    }
+	    //style.href = chrome.extension.getURL('vine.css');
+	    (document.head||document.documentElement).appendChild(style);
+	}
 
-        if($('body').hasClass('page-profile')) {
-            $(document).on('click', function(e) {
-                pageProfile();
-            });
-        }else if($('body').hasClass('page-media')) {
-            //This is a direct link to an image
-            pageMedia();
-        }
+	/*setTimeout(function() { 
+	    /*for (var i=0; i<3; i++) {
+		setTimeout(blur(i),1000);
+	    }*/
+	    //blur(2);
+	//}, 3000);
+
     });
 
     /*chrome.extension.onRequest.addListener( function(req, sender, sendResponse) {
