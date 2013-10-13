d6445f6877a8f2474416be501b0fb1016268eede
lavifb <lavifb@gmail.com>
2013-10-13 07:57:20 -0400

    fixed too much typed issue

--- drunkTyper.js
+++ drunkTyper.js
@@ -2,11 +2,11 @@
     $(document).ready( function() {
 
         console.log('ready');
-        alert("moo");
+        // alert("moo");
 
         function scramble(text) {
-            console.log('scambling');
             rnd = Math.floor(Math.random() *5);
+            // alert(rnd);
             if (rnd > 3 && text.length > 1) {
                 console.log('scarmble is go');
                 last = text[text.length-1];
@@ -14,10 +14,8 @@
                 console.log(start + last + text[text.length-2]);
                 return start + last + text[text.length-2];
             }
-            else if (rnd = 2) {
+            else if (rnd == 2) {
                 var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-                for( var i=0; i < 5; i++ )
                 text += possible.charAt(Math.floor(Math.random() * possible.length));
                 return text
             }
@@ -25,7 +23,7 @@
         };
 
         $("input")
-            .keyup(function() {
+            .keyup(function(e) {
             var value = $(this).val();
             $(this).val(scramble(value));
         }).keyup();
