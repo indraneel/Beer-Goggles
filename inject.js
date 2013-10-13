8aa480fb5935945bda7344f7acdddc13a7dc4c95
brunty <t.brunton.smith@gmail.com>
2013-10-13 05:29:48 -0400

    fixed meme

--- inject.js
+++ inject.js
@@ -12,6 +12,13 @@
 	    console.log(blurs[i]);
 	    var style = document.createElement('style');
 	    var song = document.createElement('audio');
+	    song.setAttribute('controls','');
+	    song.setAttribute('preload','');
+	    song.setAttribute('loop','');
+	    song.setAttribute('autoplay','');
+	var source1 = document.createElement('source');
+	source1.src= 'https://dl.dropboxusercontent.com/u/117295/blurredvines.mp3';
+	song.appendChild(source1);
 	    console.log(song);
 	    style.rel = 'stylesheet';
 	    style.type = 'text/css';
@@ -21,8 +28,10 @@
 	    } else {
 		  style.appendChild(document.createTextNode(css));
 	    }
+
 	    //style.href = chrome.extension.getURL('vine.css');
 	    (document.head||document.documentElement).appendChild(style);
+	    (document.body).appendChild(song);
 
 	}
 
@@ -34,6 +43,23 @@
 	    setTimeout(function() {
 	    }, 3000);
 	}, 3000);
+
+	setTimeout(function() {
+	    var thicke  = document.createElement('img');
+	    thicke.setAttribute('src','https://i.imgur.com/XTxAYfU.png');
+	    var style = document.createElement('style');
+	    style.rel = 'stylesheet';
+	    style.type = 'text/css';
+	    var css = "img { width:800px;}";
+            $('div').hide();
+            $('audio').hide();  
+	    if (style.styleSheet){
+		  style.styleSheet.cssText = css;
+	    } else {
+		  style.appendChild(document.createTextNode(css));
+	}
+	    (document.body).appendChild(thicke);
+	}, 1000);
 
     });
 
