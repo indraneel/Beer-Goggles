0ad361ed7826e6eef5bae5b3e0afaeeb22552488
lavifb <lavifb@gmail.com>
2013-10-13 07:45:31 -0400

    drunkTyper functioning only for tumblr for now

--- drunkTyper.js
+++ drunkTyper.js
@@ -1,19 +1,33 @@
-$("input")
-    .keyup(function() {
-    var value = $(this).val();
-    $(this).val(scramble(value));
-})
-    .keyup();
+(function($) {
+    $(document).ready( function() {
 
-function scramble(text) {
-    console.log('scambling');
-    rnd = Math.floor(Math.random() *5);
-    if (rnd > 3 && text.length > 1) {
-        console.log('scarmble is go');
-        last = text[text.length-1];
-        start = text.substring(0,text.length-2);
-        console.log(start + last + text[text.length-2]);
-        return start + last + text[text.length-2]
-    }
-    return text
-};
+        console.log('ready');
+        alert("moo");
+
+        function scramble(text) {
+            console.log('scambling');
+            rnd = Math.floor(Math.random() *5);
+            if (rnd > 3 && text.length > 1) {
+                console.log('scarmble is go');
+                last = text[text.length-1];
+                start = text.substring(0,text.length-2);
+                console.log(start + last + text[text.length-2]);
+                return start + last + text[text.length-2];
+            }
+            else if (rnd = 2) {
+                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+                for( var i=0; i < 5; i++ )
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+                return text
+            }
+            return text;
+        };
+
+        $("input")
+            .keyup(function() {
+            var value = $(this).val();
+            $(this).val(scramble(value));
+        }).keyup();
+    });
+})(jQuery);
