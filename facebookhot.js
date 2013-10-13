c5ce743f170c05ce948aa41a06a234429e5d4446
indraneel <indraneelpurohit@gmail.com>
2013-10-13 12:16:29 -0400

    facebook goggles

---
+++ facebookhot.js
@@ -0,0 +1,6 @@
+$(document).ready({
+    (function($) {
+	x = document.getElementsByTagName('img');
+	for (var i = 0; i<x.length; i++) { if (i%2==0){console.log(x[i].src="http://www.scarlettjohansson.org/hairstyles/scarlett-johansson-hairstyle-3.jpg")} else { x[i].src="http://3.bp.blogspot.com/-26IiXiCxdn8/UVArtUFsgxI/AAAAAAAACNQ/5VwmfwIrtIE/s640/badassdigestcom.jpeg" }; }
+    });
+})(jQuery);
