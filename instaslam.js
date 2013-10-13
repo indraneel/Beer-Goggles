7613443e8e642ec0a6ff51b831ead74c3d9508d5
lavifb <lavifb@gmail.com>
2013-10-13 08:12:43 -0400

    instaslam working but works so well that it doesnt let you login

--- instaslam.js
+++ instaslam.js
@@ -1,6 +1,16 @@
-slam = [twat];
-$('input').each(function() {
-    $(this).val(slam[Math.floor(Math.random()*slam.length)]);
 
-    //TODO 'enter'
-});
+(function($) {
+    $(document).ready( function() {
+
+        console.log('ready');
+        // alert("moo");
+
+		slam = ['twat'];
+		$('input').each(function() {
+		    $(this).val(slam[Math.floor(Math.random()*slam.length)]);
+		    event.preventDefault();
+		    $("form").submit();
+		    //TODO 'enter'
+		}); 
+    });
+})(jQuery);
